c32dc35321c33b41647948dbb077910766f74a96
ZapikDimka <130277188+ZapikDimka@users.noreply.github.com>
2024-10-15 21:52:08 +0300

    part 1

--- rustlings/exercises/quiz2.rs
+++ rustlings/exercises/quiz2.rs
@@ -7,7 +7,7 @@
 // you think each value is. That is, add either `string_slice` or `string`
 // before the parentheses on each line. If you're right, it will compile!
 
-// I AM NOT DONE
+// I AM DONE
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -17,14 +17,15 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue"); // &str
+    string("red".to_string()); // String
+    string(String::from("hi")); // String
+    string("rust is fun!".to_owned()); // String
+    string("nice weather".into()); // String
+    string(format!("Interpolation {}", "Station")); // String
+    string_slice(&String::from("abc")[0..1]); // &str (slicing the string)
+    string_slice("  hello there ".trim()); // &str (after trimming)
+    string("Happy Monday!".to_string().replace("Mon", "Tues")); // String (replace returns a String)
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase()); // String (to_lowercase returns a String)
 }
+
